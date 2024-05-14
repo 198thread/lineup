21decdcad2f90b9827e3c30630508debb895b44a
198t <kpursoty@gmail.com>
2024-05-14 15:54:48 +0100

    Update db_mgmt.py
    added clarity to comments

--- lineup_svr/db_mgmt.py
+++ lineup_svr/db_mgmt.py
@@ -6,25 +6,32 @@ db_conn = env_dials.db_conn
 index_name = env_dials.index_name
 
 def db_del_all():
-    # Connect to the local Elasticsearch instance
-    # Define the query to match all documents
+    """
+        reset database
+    """
+
+    # define match all query
     query = {
     "query": {
         "match_all": {}
     }
     }
 
-    # Perform the delete by query operation
+    # match all & delete
     response = db_conn.delete_by_query(index=index_name,
                                                 body=query,
                                                 conflicts='proceed')
 
-    # Print the response to see the result of the delete operation
+    # print records
     print(response)
 
-def db_mapping_update():
 
-    # Define the new settings and mappings for the index
+def db_mapping_update():
+    """
+        apply update to schema
+    """
+
+    # define new schema (wanted schema)
     index_settings = {
         "settings": {
             "index": {
@@ -76,31 +83,47 @@ def db_mapping_update():
     }    
     
     try:
-        # Create the new index with the specified settings and mappings
+        # attempt update
         db_conn.options(ignore_status=400).indices.create(index=index_name, body=index_settings)
 
     except:
+        # silent failure, allows for update attempt to only succeed when db == new
         pass
 
 
 
 def index_document(document, index, conn):
+    """
+        portable task for handling document input into db
+    """
+    
+    # attempt to index new document
     try:
         response = conn.index(index=index, document=document)
+
         print(f"db added face")
         return f"Document added to {index}, ID: {response['_id']}"
+
     except Exception as e:
         print(f"err, index_document: db failed to add: {str(e)}")
         return f"Failed to add document to {index}: {str(e)}"
 
 
 def db_face_add(content, index_name, db_conn):
+    """
+        meta function for creating portable tasks, to be executed
 
+        FUTURE: update this and app.face_req for better separation of concerns
+    """
+    
+    # return array
     tasks = []
 
-    # Ensure embeddings and facial_area are indexed similarly
+    # validate data via len(embeddings) == len(facial_area)
     for i in range(len(content['embeddings'])):
-        # Prepare the document to be indexed
+
+        # prep information into db format
+        # aka embedding NOT embeddingS
         data_point = {
             "hostPageURL": content['hostPageURL'],
             "imageDirectURL": content['imageDirectURL'],
@@ -109,12 +132,14 @@ def db_face_add(content, index_name, db_conn):
             "embedding": content['embeddings'][i]
         }
 
-        # Create a callable task using partial
+        # create portable task, to be fed into partial
         task = partial(index_document,
                     document=data_point,
                     index=index_name,
                     conn=db_conn)
         
+        # add to queue
         tasks.append(task)
 
+    # return task
     return tasks
