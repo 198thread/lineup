67f53dd3d0e3db7f514453ca30c9009c6b4a6da4
198t <kpursoty@gmail.com>
2024-05-14 15:53:34 +0100

    Update data_parse.py
    added clarity to comments

--- lineup_svr/data_parse.py
+++ lineup_svr/data_parse.py
@@ -1,32 +1,40 @@
 import base64
+from deepface import DeepFace
 from io import BytesIO
 from PIL import Image
-from deepface import DeepFace
 
 def post_analysis(results, img_data):
+    """
+        extract data into format for sending over socket
+    """
 
+    # holder variables, to be returned
     face_embeddings = []
     facial_data = []
 
+    # only process if faces found
     for face in results:
 
+        # unpack from model data
         facial_area = face['facial_area']
         embedding = face['embedding']
 
         try:
-            # Decode the base64 string
+            # decode the base64 string
             mimeless_image = img_data.split('base64,')[1]
             image_data = base64.b64decode(mimeless_image)
             
-            # Open the image using PIL
+            # open the image using PIL
             image = Image.open(BytesIO(image_data))
             
-            # Get dimensions
+            # get dimensions
             width, height = image.size
 
+            # calculate appropriate place for html button
             centre_x = facial_area['x'] + (facial_area['w'] / 1.2)
             centre_y = facial_area['y'] + (facial_area['h'] / 20)
 
+            # pack location data into one dict
             facial_returns = {
                 'x': facial_area['x'],
                 'y': facial_area['y'],
@@ -36,6 +44,7 @@ def post_analysis(results, img_data):
                 'y_per': (centre_y / height) * 100
             }
 
+            # add per-face data to return array
             facial_data.append(facial_returns)
             face_embeddings.append(embedding)
 
@@ -43,4 +52,3 @@ def post_analysis(results, img_data):
             print(f'Error in post_analysis: {e}')
 
     return facial_data, face_embeddings
-
