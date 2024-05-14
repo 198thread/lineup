8a8d8bd073e18156575276a12a4497169c990cea
198t <kpursoty@gmail.com>
2024-05-14 15:55:38 +0100

    Update env_dials.py
    added clarity to comments

--- lineup_svr/env_dials.py
+++ lineup_svr/env_dials.py
@@ -1,10 +1,20 @@
 import os
 from elasticsearch import Elasticsearch
 
-# Get environment variables
+"""
+    useful metadata for shared data
+    WARN: singleton pattern
+    
+    FUTURE: use for containerised options storage
+"""
+# get name of db
 env_conn = os.getenv('DB_CONN', 'http://localhost:9200') 
 
+# create connection
 db_conn = Elasticsearch([env_conn])
 
+# get name of index
 index_name = os.getenv('INDEX_NAME', 'lineup_faces')
+
+# set a min_score, currently unused
 min_score = float(os.getenv('MIN_SCORE', '1.54')) 
